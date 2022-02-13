215c6582e50360612784d58e91dba1a3f532c8da
fujiyamaorange <kaito.bakuman@gmail.com>
2022-02-13 11:45:13 +0900

    ✨ add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import { FormikForm } from './form/FormikForm'
+import { ReactHookForm } from './form/ReactHookForm'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <header>This is Header</header>
+      <Routes>
+        <Route path="/">
+          <Route path="form">
+            <Route path="formik" element={<FormikForm />} />
+            <Route path="reacthookform" element={<ReactHookForm />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
